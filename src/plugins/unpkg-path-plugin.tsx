34218807e35144c5d74a96cffd480ec540f76473
Todd Garrison <toddlgarrison@gmail.com>
2024-03-28 16:21:23 -0400

    implement iterative approach to search

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -9,12 +9,27 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return { 
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', 
-            namespace: 'a', 
-          };
+        } 
+
+        if (args.path.includes('./') || args.path.includes('../')){
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer  + '/').href
+          }
         }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        }
+
+
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return { 
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', 
+        //     namespace: 'a', 
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -24,15 +39,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
           };
         }
 
         const { data } = await axios.get(args.path)
-        console.log(data);
-
+        return {
+          loader: 'jsx',
+          contents: data
+        }
       });
     },
   };
