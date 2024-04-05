99876a441112d971982c33c0d64f3568a7ed0419
Todd Garrison <toddlgarrison@gmail.com>
2024-04-05 14:21:19 -0400

    Implement fix for Nested Paths and test

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -22,14 +22,6 @@ export const unpkgPathPlugin = () => {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
         }
-
-
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return { 
-        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', 
-        //     namespace: 'a', 
-        //   };
-        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -39,13 +31,14 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path)
+        const { data, request } = await axios.get(args.path)
+        console.log(request)
         return {
           loader: 'jsx',
           contents: data
