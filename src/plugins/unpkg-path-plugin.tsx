e8e85e7de6474767ac25fbf575dc94e32e96edbe
Todd Garrison <toddlgarrison@gmail.com>
2024-04-12 13:49:35 -0400

    Fixed: TypeScript error

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -42,7 +42,7 @@ export const unpkgPathPlugin = () => {
           };
         }
 
-        const cachedResult = await fileCache.getItem(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
         if(cachedResult) {
           return cachedResult
@@ -50,7 +50,7 @@ export const unpkgPathPlugin = () => {
 
         const { data, request } = await axios.get(args.path)
         
-        const result = {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
