aabcea6f055bdcd2e913d1e802c6eea1a6d267d1
Todd Garrison <toddlgarrison@gmail.com>
2024-04-25 17:46:36 -0400

    Fix: Typo

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugging';
+import { fetchPlugin } from './plugins/fetch-pluggin';
 
 const App = () => {
     const ref = useRef<any>();
