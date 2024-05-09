6474e6abbfdbb39f60e89943b9d9c524a03994b2
Todd Garrison <toddlgarrison@gmail.com>
2024-05-09 16:31:22 -0400

    Refactor out Preview code

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
-
+import Preview from './components/preview';
 
 const App = () => {
     const ref = useRef<any>();
-    const iframe = useRef<any>();
+    const [code, setCode] = useState('');
     const [input, setInput] = useState('');
 
     const startService = async () => {
@@ -28,8 +28,6 @@ const App = () => {
             return;
         }
 
-        iframe.current.srcdoc = html;
-
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -44,29 +42,9 @@ const App = () => {
             }
         })
 
-        // setCode(result.outputFiles[0].text)
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+        setCode(result.outputFiles[0].text)
+        
     };
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try{
-                            eval(event.data)
-                        } catch(error) {
-                            const root = document.querySelector('#root');
-                            root.innerHTML ='<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>'
-                            console.error(error)
-                        }
-                    }, false);
-                </script>
-            </body>
-        </html>
-        `
     
     return (
         <div>
@@ -81,7 +59,7 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+            <Preview code={code} />
         </div>)
 };
 
