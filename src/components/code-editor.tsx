ec3e6cc2cdcc7dcdb2dd015cd94bfe59b7b5ce62
Todd Garrison <toddlgarrison@gmail.com>
2024-05-09 16:10:17 -0400

    Implement highlighting css

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -3,6 +3,8 @@ import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react'
 import prettier from 'prettier'
 import parser from 'prettier/parser-babel'
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
 
 interface CodeEditorProps {
     initialValue: string;
@@ -19,7 +21,15 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
         })
 
         monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
-    }
+
+        const highlighter = new Highlighter(
+            // @ts-ignore
+            window.monaco,
+            codeShift,
+            monacoEditor
+        );
+        highlighter.highLightOnDidChangeModelContent();
+    };
 
     const onFormatClick = () => {
         const unformatted = editorRef.current.getModel().getValue()
