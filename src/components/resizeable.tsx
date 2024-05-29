0c95da437c21669404533033862537f7ab97309f
Todd Garrison <toddlgarrison@gmail.com>
2024-05-29 17:15:29 -0400

    Implement window resize lag issue

--- src/components/resizeable.tsx
+++ src/components/resizeable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 interface ResizableProps {
@@ -8,20 +9,44 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     let resizableProps: ResizableBoxProps;
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight)
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        let timer: any;
+        const listener = () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                setInnerHeight(window.innerHeight)
+                setInnerWidth(window.innerWidth)
+                if (window.innerWidth * 0.75 < innerWidth) {
+                    setInnerWidth(Math.floor(window.innerWidth * 0.75))
+                }
+            }, 100)
+        }
+        
+        window.addEventListener('resize', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener);
+        }
+    }, [])
 
     if (direction === "horizontal") {
         resizableProps = {
             className: "resize-horizontal",
-            minConstraints:[Math.floor(window.innerWidth * 0.2), Infinity],
-            maxConstraints:[Math.floor(window.innerWidth * 0.75), Infinity],
+            minConstraints:[Math.floor(innerWidth * 0.2), Infinity],
+            maxConstraints:[Math.floor(innerWidth * 0.75), Infinity],
             height:Infinity,
-            width: window.innerWidth * 0.75,
+            width: innerWidth * 0.75,
             resizeHandles:['e'],
         }
     } else {
         resizableProps = {
             minConstraints:[Infinity, 24],
-            maxConstraints:[Infinity, Math.floor(window.innerHeight * 0.9)],
+            maxConstraints:[Infinity, Math.floor(innerHeight * 0.9)],
             height:300,
             width:Infinity,
             resizeHandles:['s'],
