6c713fa45a3888db5ebc925f03a31a2e49e71370
Todd Garrison <toddlgarrison@gmail.com>
2024-05-29 17:49:28 -0400

    Implement debouncing logic

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
-import Resizable from './resizeable';
+import Resizable from './resizable';
 
 const CodeCell = () => {
     const [code, setCode] = useState('');
     const [input, setInput] = useState('');
 
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            const output = await bundle(input);
+            setCode(output);
+        }, 750);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [input]);
     
     return (
         <Resizable direction="vertical">
