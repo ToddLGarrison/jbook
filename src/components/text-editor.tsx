1a8f7cc0d047c4d1c9f845f1bfd8c765734f3ee0
Todd Garrison <toddlgarrison@gmail.com>
2024-06-03 15:03:59 -0400

    Implement textEditor toggle

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = (() => {
     const [editing, setEditing] = useState(false);
+
+    useEffect(() => {
+        const listener = (event: MouseEvent) => {
+            setEditing(false);
+        }
+        document.addEventListener('click', listener, { capture: true });
+
+        return () => {
+            document.removeEventListener('click', listener, { capture: true });
+        };
+    }, []);
 
     if (editing) {
         return (
