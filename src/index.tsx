239556882a0d020e0e92aa11af9529c6a0a130b5
Todd Garrison <toddlgarrison@gmail.com>
2024-06-05 16:51:16 -0400

    Connect React to Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom';
 // import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import TextEditor from './components/text-editor';
 
 const App = () => {
     return (
-        <div>
-            <TextEditor />
-        </div>)
+        <Provider store={store}>
+            <div>
+                <TextEditor />
+            </div>
+        </Provider>
+    );
 };
 
 ReactDOM.render(
