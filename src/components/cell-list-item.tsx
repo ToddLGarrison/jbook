16159236e49bf8e048798b5285fab47d833fff9d
Todd Garrison <toddlgarrison@gmail.com>
2024-06-10 16:05:53 -0400

    Implement Action Bar

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,6 +1,7 @@
 import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
+import ActionBar from "./action-bar";
 
 interface CellListItemProps {
     cell: Cell
@@ -16,7 +17,10 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
 
 
     return (
-        <div>{child}</div>
+        <div>
+            <ActionBar id={cell.id}/>
+            {child}
+        </div>
     )
 };
 
