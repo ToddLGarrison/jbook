cc8e9e53a7a9e5c17ec6397be4ad3b2d50531ba4
Todd Garrison <toddlgarrison@gmail.com>
2024-06-12 16:28:57 -0400

    Update actionbar css

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,3 +1,4 @@
+import './action-bar.css'
 import { useActions } from "../hooks/use-actions";
 
 interface ActionBarProps {
@@ -9,7 +10,7 @@ const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
 
 
     return (
-        <div>
+        <div className="action-bar">
             <button className="button is-primary is-small" onClick={() => moveCell(id, 'up')}>
                 <span className="icon">
                     <i className="fas fa-arrow-up"></i>
--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,3 +1,4 @@
+import './cell-list-item.css'
 import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
@@ -17,9 +18,9 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
 
 
     return (
-        <div>
-            <ActionBar id={cell.id}/>
+        <div className="cell-list-item">
             {child}
+            <ActionBar id={cell.id}/>
         </div>
     )
 };
