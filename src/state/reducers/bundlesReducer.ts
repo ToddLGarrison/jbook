85bae162581acc5e79d1e6d5c64b0166cce358d7
Todd Garrison <toddlgarrison@gmail.com>
2024-06-17 11:35:36 -0400

    implement bundleReducer

---
+++ src/state/reducers/bundlesReducer.ts
@@ -0,0 +1,36 @@
+import { produce } from "immer";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface BundlesState {
+    [key: string]: {
+        loading: boolean;
+        code: string;
+        error: string;
+    }
+}
+
+const initialState: BundlesState = {};
+
+const reducer = produce((state: BundlesState = initialState, action: Action): BundlesState => {
+    switch (action.type) {
+        case ActionType.BUNDLE_START:
+            state[action.payload.cellId] = {
+                loading: true,
+                code: '',
+                error: ''
+            }
+            return state;
+        case ActionType.BUNDLE_COMPLETE:
+            state[action.payload.cellId] = {
+                loading: false,
+                code: action.payload.bundle.code,
+                error: action.payload.bundle.error
+            }
+            return state;
+        default:
+            return state;
+    }
+})
+
+export default reducer
