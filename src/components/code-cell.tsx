348d543247fff643fc66d62cdfbd5bf6d94c3bfe
Todd Garrison <toddlgarrison@gmail.com>
2024-06-19 17:34:20 -0400

    Implement background fix

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -39,15 +40,16 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell } ) => {
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-                {
-                    !bundle || bundle.loading 
-                        ? <div className='progress-cover'>
-                            <progress className='progress is-small is-primary' max='100'>
-                                Loading
-                            </progress>
-                        </div>
-                        : <Preview code={bundle.code} error={bundle.error} />
-                }
+                <div className='progress-wrapper'>
+                    {!bundle || bundle.loading ? (
+                            <div className='progress-cover'>
+                                <progress className='progress is-small is-primary' max='100'>
+                                    Loading
+                                </progress>
+                            </div>
+                        ) : ( <Preview code={bundle.code} error={bundle.error} />
+                        )}
+                </div>
             </div>
         </Resizable>
     );
