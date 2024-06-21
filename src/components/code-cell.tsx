af79267d60a69972327fbb2d7805be2775c1e428
Todd Garrison <toddlgarrison@gmail.com>
2024-06-21 15:14:54 -0400

    Implement cumulativeCode

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -14,6 +14,22 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell } ) => {
     const { updateCell, createBundle } = useActions();
     const bundle = useTypedSelector((state) => state.bundles[cell.id])
+    const cumulativeCode = useTypedSelector((state) => {
+        const { data, order } = state.cells;
+        const orderedCells = order.map(id => data[id])
+
+        const cumulativeCode = [];
+
+        for (let c of orderedCells) {
+            if(c.type === 'code') {
+                cumulativeCode.push(c.content);
+            }
+            if (c.id === cell.id) {
+                break;
+            }
+        }
+        return cumulativeCode;
+    });
 
     useEffect(() => {
         if(!bundle) {
