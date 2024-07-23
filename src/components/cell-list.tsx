5f2bbceb11916d0e9a584893fb69f6620f205fd5
Todd Garrison <toddlgarrison@gmail.com>
2024-07-23 11:38:18 -0400

    implement persistMiddleware

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,11 +1,22 @@
 import './cell-list.css';
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useTypedSelector } from "../hooks/use-type-selector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
+import { useActions } from '../hooks/use-actions';
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
+
+    const { fetchCells, saveCells } = useActions();
+
+    useEffect(() => {
+        fetchCells();
+    }, []);
+
+    useEffect(() => {
+        saveCells();
+    }, []);
 
     const renderedCells = cells.map((cell) => (
         <Fragment key={cell.id}>
